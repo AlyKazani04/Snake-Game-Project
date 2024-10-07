201bf1614d85443825380570c036745308e92530
AlyKazani04 <aly.kazani52@gmail.com>
2024-10-07 22:11:20 +0500

    Snake Game Plan
    Please go through this and if you have any suggestions then make a comment and commit or msg on whtsapp grp and we can discuss.

---
+++ Snake_Plan.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define rows 20
+#define cols 20
+
+char board[rows * cols]; //board initialise
+int isgameover = 0; //game over flag: off
+
+//game over check func
+
+
+//board function
+
+
+//draw snake func  snake head ='@' , snake body ='*'
+
+
+//snake movement func
+
+
+//setup snake spawn point = random
+
+
+//create food func  food ='+'
+
+
+
+//setup food at random place func
+
+
+
+//input reading func
+
+
+//game rule func with all rules
+//consuming food increases snake length and gives score, going over border ends game, going over body ends game
+
+
+//final all function calling function (main)
+
+
+
+//example expected output
+#############
+#			#
+#			#
+#	+		#
+#		@	#
+#		*	#
+#		*	#
+#			#
+#			#
+#		+	#
+#			#
+#	+		#
+#			#
+#############
